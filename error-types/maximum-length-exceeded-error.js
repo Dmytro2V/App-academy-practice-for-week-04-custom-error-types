92616e57cd9e541b2f7e21dd81d4fabbee9fb812
Dmytro2V <op2vin@gmail.com>
2022-10-04 23:27:06 +0300

    done

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -1,6 +1,24 @@
 const ValidationError = require('./validation-error');
 
 // Your code here
+class MaximumLengthExceededError extends ValidationError {
+  constructor (exceededLength , ...params) {
+    super ( ...params)
+    this.exceededLength = exceededLength;
+    if (exceededLength) {
+      this.message = `Maximum length exceeded by ${exceededLength}`
+    } else {
+      this.message = `Maximum length exceeded`;
+    }  
+    // Maintains proper stack trace for where error was thrown (available on V8)
+    //if (Error.captureStackTrace) {
+    //  Error.captureStackTrace(this,  this.name = 'MaximumLengthExceededError');
+    //} - not needed in child if exist in parent
+
+    // The name property should match the class's name
+    this.name = 'MaximumLengthExceededError'    
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -1,5 +1,18 @@
 // Your code here
+class ValidationError extends Error {
+  constructor (message = 'Invalid input', ...params) {
+    super (message, ...params)
+    
 
+    // Maintains proper stack trace for where error was thrown (available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationError);
+    }
+
+    // The name property should match the class's name
+    this.name = 'ValidationError';
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
